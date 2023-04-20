81089c8b42947e4c1add9d4259c97daef5c1adf0
Eri4ka <navyhasanov@gmail.com>
2023-04-20 11:57:31 +0500

    fix config

--- next.config.js
+++ next.config.js
@@ -31,6 +31,16 @@ const nextConfig = {
       },
     ],
   },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/offer': { page: '/offer' },
+      '/contacts': { page: '/contacts' },
+    };
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
